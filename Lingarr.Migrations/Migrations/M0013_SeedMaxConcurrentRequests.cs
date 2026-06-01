78d9b23e3eb760bce897f4c7e3a0e19dcab66d94
Matias Maquiel <m.maquiel1991@gmail.com>
2026-06-01 09:58:51 +0200

    feat: seed max_concurrent_requests setting with default 1

---
+++ Lingarr.Migrations/Migrations/M0013_SeedMaxConcurrentRequests.cs
@@ -0,0 +1,17 @@
+using FluentMigrator;
+
+namespace Lingarr.Migrations.Migrations;
+
+[Migration(13)]
+public class M0013_SeedMaxConcurrentRequests : Migration
+{
+    public override void Up()
+    {
+        Insert.IntoTable("settings").Row(new { key = "max_concurrent_requests", value = "1" });
+    }
+
+    public override void Down()
+    {
+        Delete.FromTable("settings").Row(new { key = "max_concurrent_requests" });
+    }
+}
